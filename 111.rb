1ffc96e0cbebd57b101ee6c7db2c105103c66491
BuickSalesman <adam.odonnell.e@gmail.com>
2023-07-18 17:47:05 -0400

    111

---
+++ 111.rb
@@ -0,0 +1,27 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+puts 1 + 3
+puts 70 - 30
+puts 3 * 4
+puts 1966 / 2
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+Puts 1 + 3 # error caused by purposeful capitalization
+putts 70 - 30 # error caused by purposeful mispelling
+puts 3 x 4 # error cause by purposeful conflation of multiplication symbols
+puts 1966 / 0 # error caused by dividing by zero
+# 111.rb: --> 111.rb
+# syntax error, unexpected local variable or method, expecting end-of-input
+# >  4  puts 1 + 3
+# >  5  puts 70 - 30
+# >  6  puts 3 * 4
+# >  7  puts 1966 / 2
+# > 12  Puts 1 + 3 
+# > 13  putts 70 - 30
+# > 14  puts 3 x 4
+# 111.rb:14: syntax error, unexpected local variable or method, expecting end-of-input (SyntaxError)
+# puts 3 x 4 
+#        ^ 
