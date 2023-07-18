b24f0626a0ee827f43a5d257868519b4ae4379c4
BuickSalesman <adam.odonnell.e@gmail.com>
2023-07-18 19:54:20 -0400

    Create 121.rb

---
+++ 121.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+# Write example code to demonstrate three string methods you've never 
+# worked with before from the Ruby documentation. 
+# Write a comment for each method describing how it works in your own words.
+puts "hello".upcase # capitilizes letters within string
+puts "hello".reverse # reverses the string
+puts "hello".length # calculates number of letters within string
+
+# QUESTION 2
+# Write example code that crashes using an integer method from the Ruby documentation. 
+# Write a comment describing the error message in your own words.
+puts "hello".ceil #crashes because cannot round up without any numbers
