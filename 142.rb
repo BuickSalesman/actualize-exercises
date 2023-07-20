16ef88612acb2c047d500ed802618c250565e223
BuickSalesman <adam.odonnell.e@gmail.com>
2023-07-20 18:01:04 -0400

    Create 142.rb

---
+++ 142.rb
@@ -0,0 +1,23 @@
+# QUESTION 1
+# Write a program that starts with an array of strings, 
+# then prints out each string on separate lines using a while loop.
+
+fruits = ["kiwi", "watermelon", "strawberry", "mango"]
+index = 0
+while index < fruits.length
+    puts fruits[index]
+    index = index + 1
+end
+
+# QUESTION 2
+# Write a program that starts with an array of numbers, 
+# then prints out each number times 3 using a while loop.
+
+numbers = [45, 67, 23, 99]
+index = 0
+while index < numbers.length
+    puts numbers[index] * 3
+    index = index + 1
+end
+
+
