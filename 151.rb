e3ae90a353eed71185b88f4be79ee3911b0e46bc
BuickSalesman <adam.odonnell.e@gmail.com>
2023-07-21 20:37:08 -0400

    Create 151.rb

---
+++ 151.rb
@@ -0,0 +1,66 @@
+# QUESTION 1
+# Write a method that prints out any message or design that you choose
+# (you can use an ASCII art generator if you want to make it fancy).
+# Then run the method three times to see your message display three times.
+
+def childhood
+    puts "              _ "
+    puts "             /_\ "
+    puts "_            )_(            _ "
+    puts "|`-.___,.-~'`|=|`'~-.,___,-'| "
+    puts "|  __________|=|__________  | "
+    puts "| |    ______|=|__________| | ___      _      _  _   _             _ "
+    puts "| |   |  ____|=|_____     / |  |  |_| |_  |  |_ | _ |_ |\| |\  /\ |_ "
+    puts "| |   | /    |=|    /    /| |  |  | | |_  |_ |_ |_| |_ | | |/  \/ | "
+    puts "| |   |/   ,-|_|-. / /  /_|_|______ ______     _______        ____ "
+    puts "| |      ,' _____ / // / \    ___  |\    /     \      `.      \   \ "
+    puts "| |     / ,'| A |/ // /   |  |   \ | |  |       |  |`.  \     /    \ "
+    puts "| |    /_// |/V\/ // /    |  |    \| |  |       |  |  \  \   /  /\  \ "
+    puts "| |      /__| |/  / /     |  |       |  |       |  |   \  | /  /  \  \ "
+    puts "| |     /\  | / /| /\     |  |__/|   |  |       |  |   |  ||  |    |  | "
+    puts "| |    /  \ |/ // // \    |   __ |   |  |       |  |   |  ||  |____|  | "
+    puts "| |   /    \/ |/ //   \   |  |  \|   |  |       |  |   |  ||   ____   | "
+    puts "| |  /     /    //     \  |  |       |  |       |  |   /  ||  |    |  | "
+    puts "| | /     / /  /|       \ |  |    /| |  |    /| |  |  /  / |  |    |  | "
+    puts "| |/_____/ // / |________\|  |___/ | |  |___/ | |  |,'  /  |  |    |  | "
+    puts "| |     / // /| |        /_________|/_________|/______,'  /____\  /____\ "
+    puts "\ \    / // / | |       /|/ /_               ___  ___    _   _  _  _ ___ "
+    puts " \ \  /  / /| | |______/ | //_\  |  | |\| |/  |/\  | |_||_  |_)/_\(_` | "
+    puts "  \ \/______| | |________|/ | |  |_ | | | |\  |\/  | | ||_  |  | |._) | "
+    puts "   `.`.     | | |     ,',' "
+    puts "     `.`.   | | |   ,','    _        _        _        _ "
+    puts "       `.`-.| | |,-','     |.\      |.\      |.\      |.\   "
+    puts "         `-.| | |,-'        \\\      \\\      \\\      \\\  "
+    puts "            | | |    ________\\\______\\\______\\\______\\\_____________ "
+    puts "            | | |   | .--  __        __    ,--.        __   __   __| _  | "
+    puts "            | | |   | |-  /  \ |  | |  `   `--. | | | /  \ |  ` /  |(_` | "
+    puts "            | | |   | |   \__/ \_/| |      .__/ \/ \/ \__/ |    \__|._) | "
+    puts "            | | |   '---------------------------------------------------' "
+    puts "             \|/                  \\\ /|   \\\ /|   \\\ /|   \\\ /| "
+    puts "              V                  __\\V /  __\\V /  __\\V /  __\\V / "
+    puts "                                 \___O/   \___O/   \___O/   \___O/ "
+    puts "                                     \/\      \/\      \/\      \/\ "
+    puts "                                      \/\      \/\      \/\      \/\ "
+    puts "                                       (O)      (O)      (O)      (O) "
+
+end
+
+childhood
+
+#i L-O-V-E ASCII art, take me back
+#im not good enough yet to understand why some of the characters inside the quotations aren't
+#making it into the final print, but i've worked too hard on this to change it at this point.
+
+
+# QUESTION 2
+# When you run the code below, it should print lyrics. 
+# However, right now if you run the code, it doesn't print anything. 
+# Add the appropriate line of code to make the code run properly.
+def print_lyrics
+    puts "Now this is a story all about how"
+    puts "My life got flipped turned upside down"
+    puts "And I'd like to take a minute, just sit right there"
+    puts "I'll tell you how I became the prince of a town called Bel-Air"
+  end
+
+print_lyrics
