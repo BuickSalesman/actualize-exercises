1d97d2ff9c4133a0b1a838c7b15cb7a4d86786a9
BuickSalesman <adam.odonnell.e@gmail.com>
2023-07-26 17:58:26 -0400

    Add exercise solutions

---
+++ 231.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+person = { "first_name" => "Kelsey", "last_name" => "Kavanagh", "email" => "kgpk1234@aol.com"}
+pp person["first_name"]
+pp person["last_name"]
+pp person["email"]
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#took 3 attempts to solve from memory, will work on additional has/array
+#problems tomorrow
---
+++ 232.rb
@@ -0,0 +1,19 @@
+# QUESTION 1
+# Write code that prints all the years between 1980 to 2020 (including 1980 and 2020).
+
+index = 0
+year = 1980
+while index <= 40
+    puts year
+    index = index + 1
+    year = year + 1
+end
+
+# QUESTION 2
+# The following code is supposed to print the numbers 1 through 10, but it has errors.
+# Fix the errors with the code below.
+count = 0
+while count <= 10
+  puts count
+  count = count + 1
+end
